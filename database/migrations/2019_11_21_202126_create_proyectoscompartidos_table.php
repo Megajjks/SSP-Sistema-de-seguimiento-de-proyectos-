e7d0908ffdb8e2bfc152fadcc78f2e26175f22b6
LeonardoCG <andresxach7@gmail.com>
2019-11-21 16:17:04 -0600

    proyectocompartido2

---
+++ database/migrations/2019_11_21_202126_create_proyectoscompartidos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProyectoscompartidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proyectoscompartidos', function (Blueprint $table) {
+            $table->increments('id_proyecto');
+            $table->string('nombre', 100);
+            $table->text('describcion');
+            $table->integer('estado_actual');
+            $table->string('estatus', 50)->default("asignacion");
+            $table->integer('ncolaboradores')->nullable();
+            $table->date('fec_ini');
+            $table->date('fec_fin')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proyectoscompartidos');
+    }
+}
+ 
